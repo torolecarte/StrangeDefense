b9050dc23e41361d1536dd0f7b0450bc541d9114
Heitor Corrêa <5171086+torolecarte@users.noreply.github.com>
2021-10-24 19:52:35 -0300

    Created Stage Forest

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class MainMenuController : MonoBehaviour
     public void OnClick_NewGame()
     {
         Debug.Log("NewGame clicked!");
+        SceneManager.LoadSceneAsync("StageForest");
     }
     public void OnClick_Credits()
     {
@@ -16,6 +18,13 @@ public class MainMenuController : MonoBehaviour
     public void OnClick_Exit()
     {
         Debug.Log("Exit clicked!");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
 
